b4068be90280a09a3fdfda88f00e98f738f078eb
Nabeel Rana <nabwaregroup@gmail.com>
2025-06-27 16:18:22 -0700

    Add request for GitHub identity

--- app/sign-in/_components/GitHubSignInButton.tsx
+++ app/sign-in/_components/GitHubSignInButton.tsx
@@ -1,8 +1,13 @@
-export default function GitHubSignInButton() {
+interface GitHubSignInButtonProps {
+    authUrl: string;
+}
+
+export default function GitHubSignInButton({ authUrl }: GitHubSignInButtonProps) {
     return (
-        <button className="bg-black text-white font-semibold px-4 py-3 rounded-md flex items-center gap-3">
+        <a href={authUrl}
+            className="bg-black text-white font-semibold px-4 py-3 rounded-md flex items-center gap-3">
             <img src="/github-mark-white.png" alt="GitHub Logo" className="w-5 h-5" />
             <span>Sign in with GitHub</span>
-        </button>
+        </a>
     );
 }
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,10 +1,11 @@
+import { buildGitHubAuthUrl } from "./_lib/build-github-auth-url";
 import GitHubSignInButton from "./_components/GitHubSignInButton";
 
 export default function Page() {
     return (
         <div className="flex flex-col items-center mt-16">
             <h1 className="text-3xl font-bold mb-8">Sign in</h1>
-            <GitHubSignInButton />
+            <GitHubSignInButton authUrl={buildGitHubAuthUrl()} />
         </div >
     );
 }
